6c7f0da58ae12cf68f17d240383faa690bce41e2
萬俊瑋 <wei840222@gmail.com>
2019-03-24 23:45:57 +0800

    add collapse

--- web/src/components/Main.vue
+++ web/src/components/Main.vue
@@ -1,3 +1,23 @@
 <template lang="pug">
-  #main
+  el-main#main
+    el-collapse
+      el-collapse-item(v-for="(item, index) in storageList" :title="`${item.releaseName}-${item.type}`" :name="index")
+        div Status: {{ item.status }}
+        div Endpoint
+          a(:href="item.endpoint") {{ item.endpoint }}
 </template>
+
+<script>
+export default {
+  name: 'main',
+  data() {
+    return {
+      storageList: []
+    }
+  },
+  async mounted() {
+    const res = await this.$axios.get('http://localhost:8080/storage')
+    this.storageList = res.data.data
+  }
+}
+</script>
