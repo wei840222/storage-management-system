b4f0ba8a0d0d7ca65a6ee00a007569be22eb560d
wei840222 <wei840222@gmail.com>
2019-04-22 21:20:22 +0800

    fix

--- web/src/plugins/axios.js
+++ web/src/plugins/axios.js
@@ -8,11 +8,11 @@ import axios from "axios";
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
-// let config = {
-//   baseURL: "",
-//   timeout: 60 * 1000, // Timeout
-//   // withCredentials: true, // Check cross-site Access-Control
-// };
+let config = {
+  baseURL: "",
+  timeout: 60 * 1000, // Timeout
+  // withCredentials: true, // Check cross-site Access-Control
+};
 
 const _axios = axios.create(config)
 
