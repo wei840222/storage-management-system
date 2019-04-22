452474cdec2193e3c0815b190d2956cc884825cb
wei840222 <wei840222@gmail.com>
2019-04-22 21:29:00 +0800

    fix

--- web/src/plugins/axios.js
+++ web/src/plugins/axios.js
@@ -9,7 +9,7 @@ import axios from "axios";
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
-  baseURL: "",
+  baseURL: window.location.href,
   timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
