f9dc69d28a5fbc297887173eab1a34b9ed3c1157
wei840222 <wei840222@gmail.com>
2019-05-01 00:18:11 +0800

    fix

--- web/src/components/Main.vue
+++ web/src/components/Main.vue
@@ -10,9 +10,9 @@
       template(v-if="scope.row.config['persistence.storageClass'] === 'longhorn'" slot-scope="scope")
         el-popover(placement="top-start" trigger="hover" :content="getSizeString(scope.row.persistentVolumeClaim.size)+'/'+getSizeString(scope.row.persistentVolumeClaim.capacity)")
           el-progress(slot="reference" :text-inside="true" :stroke-width="18" color="#8e71c7" :percentage="getPercentage(scope.row.persistentVolumeClaim.size, scope.row.persistentVolumeClaim.capacity)")
-    el-table-column(label="Storage Class" prop="config['persistence.storageClass']" sortable)
+    el-table-column(label="Storage Class" prop="config['persistence.storageClass']")
       template(slot-scope="scope")
-        el-tag(size="mini" type="warning") {{ scope.row.config['persistence.storageClass'] }}
+        el-tag(size="mini" type="info") {{ scope.row.config['persistence.storageClass'] }}
     el-table-column(label="Endpoint" prop="endpoint.port")
       template(slot-scope="scope")
         a(v-if="scope.row.chartName === 'stable/minio'" :href="`http://${scope.row.endpoint.host}:${scope.row.endpoint.port}`" target="_blank")
